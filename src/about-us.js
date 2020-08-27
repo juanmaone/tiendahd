8b58b450a77d03a71f0afabbdcc58887f7b17bde
mfunes <jmfunes@mobydigital.com>
2020-08-27 19:32:48 -0300

    Navbar principal tienda

--- src/about-us.js
+++ src/about-us.js
@@ -3,18 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/NavBar.js'
 
-function App() {
+function AboutUs() {
   return (
     <div className="App">
     <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TIENDA HD
-        </p>    
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default AboutUs;
---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar, Form, Nav, Button } from "react-bootstrap";
+import Cart from "./CartIcon";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Navbar.Brand href="#home">Tienda HD</Navbar.Brand>
+            <Navbar.Brand href="#conditions">Condiciones</Navbar.Brand>
+            <Navbar.Brand href="#articles">Artículos</Navbar.Brand>
+            <Navbar.Brand href="#contact">Contacto</Navbar.Brand>
+            
+          </Nav>
+          <Form inline>
+            <Button variant="primary"><span>Comprar</span><Cart name="cartIcon" /></Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+export default NavBar;
